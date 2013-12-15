293dfd60e00ffce95467f64abcd5f44d67012fc6
Alex Stuckey <alex@stuckey.org.uk>
2013-12-15 22:48:07 +0000

    Adding footer content

---
+++ application/views/footer.php
@@ -0,0 +1,6 @@
+    </div> <!-- /container -->
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
